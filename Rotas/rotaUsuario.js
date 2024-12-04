9e6d78abd0036c64e06d9965fa9524b01cfab659
Rafael Cena <135291735+rafael-cena@users.noreply.github.com>
2024-12-04 15:18:55 -0300

    correcao autLogin

--- Rotas/rotaUsuario.js
+++ Rotas/rotaUsuario.js
@@ -10,6 +10,6 @@ rotaUsuario.patch("/:codigo", uCtrl.editar);
 rotaUsuario.delete("/:codigo", uCtrl.excluir);
 rotaUsuario.get("/:codigo", uCtrl.consultar);
 rotaUsuario.get("/",uCtrl.consultar);
-rotaUsuario.post('/verificarSenha', uCtrl.verificarSenha);
+rotaUsuario.post('/verificarSenha', uCtrl.autLogin);
 
 export default rotaUsuario;
